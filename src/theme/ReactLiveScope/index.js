be76442d43e8a6e905aae605b9755e3ff4097b4e
Cynthia Henaff <cynthia@henaff.io>
2022-08-23 17:05:09 +0200

    feat: Add button doc

--- src/theme/ReactLiveScope/index.js
+++ src/theme/ReactLiveScope/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import * as MargaretCore from '@margaret-ui/core';
-import { IoBulbOutline } from 'react-icons/io5';
+import { IoBulbOutline, IoChevronForwardOutline } from 'react-icons/io5';
 
 const ReactLiveScope = {
   React,
   ...React,
   ...MargaretCore,
   IoBulbOutline,
+  IoChevronForwardOutline,
 };
 
 export default ReactLiveScope;
--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import { MargaretProvider } from '@margaret-ui/core';
-import { createGlobalStyle } from 'styled-components';
 import '@fontsource/inter';
-
-const GlobalStyle = createGlobalStyle`
-body {
-  font-family: "Inter";
-
-  code {
-    border: 0;
-    color: var(--ifm-color-secondary);
-    background-color: transparent;
-    padding: 0;
-    vertical-align: unset;
-  }
-}
-`;
+import '@fontsource/fira-code';
 
 // Default implementation, that you can customize
 export default function Root({ children }) {
   return (
     <MargaretProvider theme={{}} colors={{}}>
-      <GlobalStyle />
       {children}
     </MargaretProvider>
   );
